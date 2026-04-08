3ab04cdbc80f79f1a012a64d22d51051dd6184a2
henrique <henriquesimoes3007@gmail.com>
2026-04-08 10:10:44 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+
+int main (){
+
+float x= 3.1415926;
+
+printf("%f \n %.2f\n",x,x);
+
+
+    return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+
+
+int main (){ 
+    int x=10;
+    int y=-5;
+    int z=4;
+
+printf("%i \n %i \n %05i \n",x,y,z);
+
+
+    return 0;
+}
+
+
+
+
+
+
