ad2bfde8a1ca2f2113863c4a3379169f0bc8e70f
henrique <henriquesimoes3007@gmail.com>
2026-04-29 09:39:46 -0300

    feat: conclui pratica04. Fecha #ID

---
+++ praticas/pratica04/leia_inteiros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    int num2;
+
+    printf("Digite dois numeros inteiros: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Numero 1: %d\n", num1);
+    printf("Numero 2: %d\n", num2);
+
+    return 0;
+}
